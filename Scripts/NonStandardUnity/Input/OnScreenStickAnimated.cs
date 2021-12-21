9b58dbce207e633e3a4ea4a3d42aa878eb9d1bec
Michael Vaganov <mvaganov@hotmail.com>
2021-12-20 21:43:04 -0800

    animated stick

---
+++ Scripts/NonStandardUnity/Input/OnScreenStickAnimated.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem.OnScreen;
+
+public class OnScreenStickAnimated : OnScreenStick {
+    RectTransform rt;
+    public float stickAnimationSpeed = 1024;
+    Vector2 targetPosition;
+    private void Start() {
+        rt = GetComponent<RectTransform>();
+    }
+    public void SetStickPositionFrominput(Vector2 input) {
+        targetPosition = input * movementRange;
+    }
+    private void Update() {
+        Vector2 d = targetPosition - rt.anchoredPosition;
+        if (d.SqrMagnitude() > 1) {
+            rt.anchoredPosition += d.normalized * stickAnimationSpeed * Time.deltaTime;
+        } else {
+            rt.anchoredPosition = targetPosition;
+        }
+    }
+}
